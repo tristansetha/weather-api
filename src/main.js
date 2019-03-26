62fcb592000c71e52a253c696fbe966de0148de6
Jared Hanson and Tristan Setha <jared+tristan@>
2019-03-26 11:27:23 -0700

    functional weather app utilizing weather api

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import './styles.css';
+import $ from 'jquery';
+// import  Weather  from './weather.js';
+
+
+$(document).ready(function() {
+    $('#weatherLocation').click(function() {
+      const city = $('#location').val();
+      $('#location').val("");
+  
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=982ab0a2656419ff4bb629ae52805f3c`;
+  
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          getElements(response);
+        }
+      }
+  
+      request.open("GET", url, true); //getElements(response) won’t be called until the conditional becomes true. In other words, by using a callback, we ensure the function doesn’t run until after we get a response from the server.
+      request.send();
+  
+     const getElements = function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      }
+    });
+  });
+
+//   Data nested inside a JSON object looks like this:
+//   {
+//     ...
+//     "main": {
+//           "temp": 297.15,
+//           "pressure": 1010,
+//           "humidity": 94,
+//           "temp_min": 297.15,
+//           "temp_max": 297.15
+//       },
+//   ...
+//   }
+  
+// 982ab0a2656419ff4bb629ae52805f3c
